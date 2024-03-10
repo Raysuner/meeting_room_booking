b5c7a3ee56644e894b3882241583c7fc2fa2021f
Raysuner <1669232805@qq.com>
2024-03-10 16:29:31 +0800

    feat: 全局增加拦截器、过滤器

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpTransformInterceptor } from './interceptor/http-transform/http-transform.interceptor';
+import { HttpExceptionFilter } from './filter/http-exception/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new HttpTransformInterceptor());
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
