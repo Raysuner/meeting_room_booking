4214d7e8302c9f1f29078d4dd5270e2ffa5384e1
Raysuner <1669232805@qq.com>
2024-03-10 16:29:54 +0800

    feat: 增加自定义错误码和错误消息

---
+++ src/common/constant/api-error-code.enum.ts
@@ -0,0 +1,4 @@
+export enum ApiErrorCode {
+  PERMISSION_EXISTED = 10001,
+  ROLE_EXISTED = 10002,
+}
---
+++ src/common/constant/api-error-msg.enum.ts
@@ -0,0 +1,4 @@
+export enum ApiErrorMessage {
+  PERMISSION_EXISTED = '权限已存在',
+  ROLE_EXISTED = '角色已存在',
+}
